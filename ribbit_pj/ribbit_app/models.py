c91b4654d36534be2d3fd0cdb346e7e738986df0
David Gaudreault <david.gaudreault@gmail.com>
2017-03-29 20:34:22 -0400

    Created Model

--- ribbit_pj/ribbit_app/models.py
+++ ribbit_pj/ribbit_app/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+import hashlib
 
-# Create your models here.
+class Ribbit(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
+    def gravatar_url(self):
+        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
